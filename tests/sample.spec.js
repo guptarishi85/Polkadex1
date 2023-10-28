7bd9f792798a1be514415b139c83b1134fe7133a
Naziya <naziyahasan7@gmail.com>
2023-10-28 22:19:23 +0530

    testing my feature branch

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -2,10 +2,9 @@ const{test,expect}=require('@playwright/test')
 
 test("Verify Application Title",async function({page})
 {
- await page.goto("https://orderbook.polkadex.trade/")
-
- const url=await page.url()
-
- console.log("title is "+url)
+    //opening trade page
+    await page.goto("https://orderbook.polkadex.trade/")
+    const url=await page.url()
+    console.log("title is "+url)
 
 })
